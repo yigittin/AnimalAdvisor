5fc34f14e2f12f34cd3bbd2493d2f139d420c517
yigittin <i.yigittin@hotmail.com>
2021-12-14 01:08:18 +0300

    Homepage Images Added

--- AnimalAdvisor/Controllers/HomeController.cs
+++ AnimalAdvisor/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
-﻿using AnimalAdvisor.Models;
+﻿using AnimalAdvisor.Data;
+using AnimalAdvisor.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
@@ -11,16 +13,17 @@ namespace AnimalAdvisor.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ApplicationDbContext context)
         {
-            _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
         {
-            return View();
+            var db = _context.Species.Include(f => f.Category);
+            return View(db.ToList());
         }
 
         public IActionResult Privacy()
